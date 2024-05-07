036c56dc9e0ef392b1948240927cb5069a73d91e
VuND <vund@splus-software.com.vn>
2024-05-07 17:17:16 +0700

    Revert "Feat: Atomic Design + Basic Main Page Template"
    This reverts commit 26907bb0dfbe59a8ab7449333f096b868ab6b04a.

--- app/interfaces/BlogPost.tsx
+++
@@ -1,8 +0,0 @@
-interface BlogPostInterface {
-    id: number;
-    title: string;
-    content: string;
-  }
-  
-export default BlogPostInterface;
-  
--- app/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-import BlogPostInterface from "../interfaces/BlogPost";
-
-export const MOCK_DATA: BlogPostInterface[] = [
-    { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
-    { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.' },
-  ];
